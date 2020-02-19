9b70deba55d7136fca50b666d954ebb22525c18a
Bekki Young <moonbladez@hotmail.com>
2020-02-19 18:37:51 +0100

    initial commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Pokegame from "../components/Pokegame/Pokegame"
+import "../components/index.css"
+
+const IndexPage = () => (
+  <>
+    <Pokegame />
+  </>
+)
+
+export default IndexPage
